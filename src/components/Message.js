b0ab5540e87be700a8bed08bf66f6d36f4d484e0
Aaron Watson <aaronwatson2975@gmail.com>
2023-12-19 00:10:05 -0330

    Working!

--- src/components/Message.js
+++ src/components/Message.js
@@ -12,6 +12,25 @@ export default function Message({
   setContent,
   onDelete,
 }) {
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        // Reader gives you a Base64 string on reader.result
+        const base64String = reader.result
+          .replace("data:", "")
+          .replace(/^.+,/, "");
+
+        setContent(`data:image/jpeg;base64,${base64String}`);
+      };
+      reader.onerror = () => {
+        console.error("Error occurred while reading file");
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className={classNames(className, styles.container)}>
       <div
@@ -30,18 +49,22 @@ export default function Message({
       >
         {type}
       </div>
-      <TextField
-        id="outlined-basic"
-        placeholder={`Enter a ${origin} message here.`}
-        variant="outlined"
-        fullWidth
-        maxRows={16}
-        multiline
-        value={content}
-        onChange={(e) => {
-          setContent(e.target.value);
-        }}
-      />
+      {type === "Text" ? (
+        <TextField
+          id="outlined-basic"
+          placeholder={`Enter a ${origin} message here.`}
+          variant="outlined"
+          fullWidth
+          maxRows={256}
+          multiline
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
+      ) : (
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+      )}
       <Button onClick={onDelete} color="error">
         Delete
       </Button>
--- src/pages/VisionPage.js
+++ src/pages/VisionPage.js
@@ -6,31 +6,64 @@ import MessageThread from "../components/MessageThread";
 
 export default function VisionPage({ openai }) {
   const [systemPrompt, setSystemPrompt] = useState("");
-  const [model, setModel] = useState("gpt-4-1106-preview");
+  const [model, setModel] = useState("gpt-4-vision-preview");
   const [temperature, setTemperature] = useState(1.0);
   const [maxTokens, setMaxTokens] = useState(2048);
   const [messages, setMessages] = useState([]);
   const [models, setModels] = useState([]);
 
   const prompt = useMemo(() => {
+    // This array will accumulate the formatted messages
+    const formattedMessages = [];
+
+    // Loop through all messages
+    messages.forEach((message) => {
+      // Check the type of message and format accordingly
+      if (message.type === "Text") {
+        // For text messages, just include the text in the content property
+        formattedMessages.push({
+          role: message.origin,
+          content: [{ type: "text", text: message.content }],
+        });
+      } else if (message.type === "Image") {
+        // For image messages, check the previous message type
+        // If it is also from the user, merge the content with the previous message
+        if (
+          formattedMessages.length > 0 &&
+          formattedMessages[formattedMessages.length - 1].role === "user"
+        ) {
+          // Append to the array of the last 'user' message
+          formattedMessages[formattedMessages.length - 1].content.push({
+            type: "image_url",
+            image_url: { url: message.content },
+          });
+        } else {
+          // If not adjacent or not 'user', create a new message with the image content
+          formattedMessages.push({
+            role: message.origin,
+            content: [
+              {
+                type: "image_url",
+                image_url: { url: message.content },
+              },
+            ],
+          });
+        }
+      }
+    });
+
     return {
       model,
       messages: [
         ...(systemPrompt.length
-          ? [
-              {
-                role: "system",
-                content: systemPrompt,
-              },
-            ]
+          ? [{ role: "system", content: systemPrompt }]
           : []),
-        ...messages.map((message) => ({
-          role: message.origin,
-          content: message.content,
-        })),
+        ...formattedMessages,
       ],
+      max_tokens: maxTokens,
+      temperature,
     };
-  }, [messages, model, systemPrompt]);
+  }, [maxTokens, messages, model, temperature]);
 
   useEffect(() => {
     openai.models.list().then((res) => {
@@ -40,17 +73,44 @@ export default function VisionPage({ openai }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleSubmit = () => {
-    openai.chat.completions.create(prompt).then((res) => {
-      const message = res.choices[0].message;
-      const messagesClone = JSON.parse(JSON.stringify(messages));
-      messagesClone.push({
-        origin: message.role,
+  const handleSubmit = async () => {
+    // Append an object to indicate the system is typing
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        origin: "assistant",
         type: "Text",
-        content: message.content,
-      });
-      setMessages(messagesClone);
+        content: "",
+      },
+    ]);
+
+    // Start the streaming
+    const stream = await openai.chat.completions.create({
+      ...prompt,
+      stream: true,
     });
+
+    // Keep a running total of the content that has been streamed back
+    let streamedContent = "";
+
+    for await (const chunk of stream) {
+      if (chunk.choices && chunk.choices[0]?.delta?.content) {
+        // Append the new chunk to the running total content
+        streamedContent += chunk.choices[0].delta.content;
+        // Use function form of setState to ensure we have the latest state
+        // eslint-disable-next-line no-loop-func
+        setMessages((prevMessages) => {
+          // Create a new array for the messages
+          const updatedMessages = [...prevMessages];
+          // Update only the last message, which should be the one showing the streamed content
+          if (updatedMessages.length > 0) {
+            updatedMessages[updatedMessages.length - 1].content =
+              streamedContent;
+          }
+          return updatedMessages;
+        });
+      }
+    }
   };
 
   return (
